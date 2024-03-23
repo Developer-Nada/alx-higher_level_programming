9171432bd75366306ce49c186a355389ff839b3c
Ma-Nada <nadamaach17@gmail.com>
2024-03-23 06:18:30 -0700

    task 0 1

---
+++ 0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrixe = matrix.copy()
+    for i in range(len(matrix))
+    matrixe[i] = list(map(lambda x: x**2, matix[i]))
+    return (matrixe)

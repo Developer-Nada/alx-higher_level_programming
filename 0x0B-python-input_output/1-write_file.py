24b63270e16408d10b11bf1a8e715b2af9961915
Ma-Nada <nadamaach17@gmail.com>
2024-04-17 17:47:32 -0700

    tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
     """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
-        print(len(text), end="")
+        return len(text)
